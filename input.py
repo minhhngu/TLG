6addfadbd14e14fc21d2840954ba418d1a5c3f4a
Minh Ngu <minhhngu@users.noreply.github.com>
2021-08-09 20:59:26 +0000

    adds input challenge

---
+++ input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter an IPv4 IP address: ")
+user_input2 = input("What is the device\'s vendor name: ")
+
+print(f'You told me the IPv4 address is:{user_input}')
+print(f'The vendor name of the device is:{user_input2}')
