f694eeb7914493672bd27e5a75b7ecda08d721df
minhhngu <56049328+minhhngu@users.noreply.github.com>
2021-08-20 09:06:16 -0700

    updates ufo.py

--- ufoproject/ufo.py
+++ ufoproject/ufo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""
 
-import numpy as np
+"""
+
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
 
 data = pd.read_csv('ufo_sighting_data.csv')
 
@@ -18,19 +19,17 @@ data_merged = data_map.set_index('STUSPS').join(data_state.set_index('states'))
 
 var = 'counts'
 
-fig, ax = plt.subplots(1, figsize=(10, 6))
+fig, ax = plt.subplots(1, figsize=(16, 10))
 plt.ylim(22, 51)
 plt.xlim(-130, -65)
-data_merged.plot(column=var,
-                 legend=True,
-                 marker='.',
-                 markersize=20,
-                 cmap='Purples',
-                 linewidth=0.8,
-                 ax=ax,
-                 edgecolor='0.8')
+plt.axis("off")
+data_merged.plot(column=var, legend=True, marker='.', markersize=20,
+                 cmap='Purples', linewidth=0.8, ax=ax, edgecolor='0.8')
 
-ax.legend(bbox_to_anchor=(1.4, 1.25), prop={'size': 12}, title="Number of Sightings")
+data_merged.apply(lambda x: ax.annotate(text=x.NAME, xy=x.geometry.centroid.coords[0],
+                                        ha='center', fontsize=5), axis=1)
+
+ax.legend(bbox_to_anchor=(1.4, 1.25), prop={'size': 8}, title="Number of Sightings")
 ax.set(title="UFO Sightings Heatmap")
 
 plt.show()
